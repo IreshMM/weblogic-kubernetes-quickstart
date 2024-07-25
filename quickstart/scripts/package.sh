690d7f6b2f8dc966ec060728624e28a5e8153bf5
Iresh Madhusanka <iresh@itz-550004apw0-wel1.dte.demo.ibmcloud.com>
2024-07-25 08:46:18 +0000

    fix package_cmd; add cache refreshing of imagetool

--- quickstart/scripts/package.sh
+++ quickstart/scripts/package.sh
@@ -4,13 +4,22 @@ SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd )"
 
 # Package the application
 jar cvf /tmp/archive.zip -C ${SCRIPT_PATH}/.. wlsdeploy
+IMAGETOOL_PATH="${SCRIPT_PATH}/../tools/imagetool/bin/imagetool.sh"
 
+CLEAR_CACHE_CMD="${IMAGETOOL_PATH} cache deleteEntry \
+    --key wdt_latest"
+ADD_INSTALLER_CMD="${IMAGETOOL_PATH} cache addInstaller \
+    --type wdt \
+    --version latest \
+    --path ${SCRIPT_PATH}/../tools/weblogic-deploy.zip"
 
-PACKAGE_CMD="${SCRIPT_PATH}/../tools/imagetool/bin/imagetool.sh \
+set -x; $CLEAR_CACHE_CMD; $ADD_INSTALLER_CMD; set +x
+
+PACKAGE_CMD="${IMAGETOOL_PATH} \
     createAuxImage \
     --tag docker.io/ireshmm/wls-quickstart:1.0 \
-    --wdtModel ${SCRIPT_PATH}/../model.yaml \
-    --wdtVariables ${SCRIPT_PATH}/../variables.properties \
+    --wdtModel ${SCRIPT_PATH}/../model/model.yaml \
+    --wdtVariables ${SCRIPT_PATH}/../model/model.properties \
     --wdtArchive /tmp/archive.zip"
     
-echo $PACKAGE_CMD
+set -x; $PACKAGE_CMD; set +x
